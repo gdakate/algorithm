f43089dce025e8a24cd55f9d16c80e64e7a228bc
gdakate <gdakate1215@gmail.com>
2022-03-16 16:08:48 +0900

    COMMIT

--- JAVA/src/BOJ9095.java
+++ JAVA/src/BOJ9095.java
@@ -1,32 +1,24 @@
 import java.util.Scanner;
 
 public class BOJ9095 {
-    public static int[] dp;
-    public static int recursive(int num){
-        if (num<=1)
-            return 0;
-
+    static int dp[]=new int[12];
+    static int recursive(int num){
         if(num==1)
             return 1;
         else if(num==2)
             return 2;
         else if(num==3)
             return 4;
-
         dp[num]=recursive(num-1)+recursive(num-2)+recursive(num-3);
-
         return dp[num];
     }
-
     public static void main(String[] args) {
+
         Scanner sc= new Scanner(System.in);
         int N=sc.nextInt();
         sc.close();
 
-        dp=new int[N];
-
-        int result=recursive(N);
-        System.out.println(result);
+        System.out.println(recursive(N));
 
     }
 
