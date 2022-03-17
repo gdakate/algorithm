cea9cfaf39209b66f334a043485d885e94c9e9e7
gdakate <gdakate1215@gmail.com>
2022-03-18 00:08:58 +0900

    최종

--- JAVA/src/BOJ11727.java
+++ JAVA/src/BOJ11727.java
@@ -1,20 +1,21 @@
-import java.util.Scanner;
+import java.io.*;
+
 
 public class BOJ11727 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt();
-        sc.close();
-        int dp[]=new int[n+1]; //이걸 왜 다 해주는건지?
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(bf.readLine());
+        int dp[]=new int[n+1];
         dp[0]=1;
         dp[1]=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]*2;
+            dp[i]=(dp[i-1]+dp[i-2]*2)%10007;
         }
-        dp[n]%=10007;
+
 
         System.out.println(dp[n]);
+
     }
 }
