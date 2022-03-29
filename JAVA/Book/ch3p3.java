b7f9b0b442a278a5a1052b1881e41ebe09ba8a4d
gdakate <gdakate125@gmail.com>
2022-03-29 23:38:59 +0900

    03.29

---
+++ JAVA/Book/ch3p3.java
@@ -0,0 +1,47 @@
+// 숫자 카드 게임
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ch3p3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(bf.readLine());
+        int N= Integer.parseInt(st.nextToken());
+        int M= Integer.parseInt(st.nextToken());
+
+        int[][] card=new int[N][M];
+
+        for(int i=0;i<N;i++){
+            StringTokenizer st1=new StringTokenizer(bf.readLine());
+            for(int j=0;j<M;j++){
+                card[i][j]=Integer.parseInt(st1.nextToken());
+            }
+        }
+
+        int [] min=new int[3];
+
+        for(int i=0;i<N;i++){
+            int  Min=10;
+            for(int j=0;j<M;j++){
+                if(card[i][j]<Min)
+                    Min=card[i][j];
+            }
+            min[i]=Min;
+        }
+
+        int Max=0;
+        for(int i=0;i<N;i++){
+            if(min[i]>Max)
+                Max=min[i];
+        }
+
+        System.out.println(Max);
+
+
+
+
+    }
+}
---
+++ JAVA/Book/ch3p4.java
@@ -0,0 +1,31 @@
+//1이 될때까지
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ch3p4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int N=Integer.parseInt(st.nextToken(br.readLine()));
+        int K=Integer.parseInt(st.nextToken(br.readLine()));
+        int count=0;
+
+        while (true){
+            if(N%K==0){
+                N/=K;
+                count++;
+            }
+            else if(N==1)
+                break;
+            else{
+                N-=1;
+                count++;
+            }
+            System.out.println(N);
+        }
+        System.out.println(count);
+    }
+}
