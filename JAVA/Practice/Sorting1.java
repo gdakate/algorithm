9ced44430fc7ec4dce8151eeb2c4c30aead5b763
gdakate <gdakate1215@gmail.com>
2022-04-06 02:02:04 +0900

    practice

---
+++ JAVA/Practice/Sorting1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Sorting1 {
+    class Solution{
+        public int[] solution(int[] array, int[][]commands){
+           int[] answer = new int[commands.length];
+
+           for(int i=0;i<commands.length;i++){
+               //j-i 크기의 새로운 배열 만듬
+               int[] temp= new int[commands[i][1]-(commands[i][0]-1)];
+               for(int j=0;j<temp.length;j++){
+                   //commands[i][0]=k
+                    temp[j]=array[j+(commands[i][0]-1)];
+               }
+               //정렬
+               Arrays.sort(temp);
+               answer[i]=temp[commands[i][2]-1];
+           }
+           return answer;
+        }
+    }
+}
